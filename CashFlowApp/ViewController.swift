56f742bc1b67470e3629c74b8bdd574e7c150f60
Zach Moore <zachmoore@Zachs-MacBook-Pro.local>
2015-01-05 13:54:36 -0700

    Initial Commit

---
+++ CashFlowApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CashFlowApp
+//
+//  Created by Zach Moore on 1/5/15.
+//  Copyright (c) 2015 Zach Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
